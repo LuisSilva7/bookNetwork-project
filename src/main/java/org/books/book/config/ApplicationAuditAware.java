ca60f1c0d458fd2640a195aa2a1f2bda7cba6e0b
LuisSilva7 <lmpsilva7@gmail.com>
2024-12-07 18:21:04 +0000

    Some fixs

--- src/main/java/org/books/book/config/ApplicationAuditAware.java
+++ src/main/java/org/books/book/config/ApplicationAuditAware.java
@@ -8,9 +8,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
-public class ApplicationAuditAware implements AuditorAware<Integer> {
+public class ApplicationAuditAware implements AuditorAware<String> {
     @Override
-    public Optional<Integer> getCurrentAuditor() {
+    public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null ||
                 !authentication.isAuthenticated() ||
@@ -20,6 +20,6 @@ public class ApplicationAuditAware implements AuditorAware<Integer> {
 
         User userPrincipal = (User) authentication.getPrincipal();
 
-        return Optional.ofNullable(userPrincipal.getId());
+        return Optional.ofNullable(userPrincipal.getName());
     }
 }
